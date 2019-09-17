41a1a1c92449521f141da748404df8be7aba2c5a
hi-jin <crushed7@o.cnu.ac.kr>
2019-09-17 10:37:18 +0900

    FIX - MyMath small errors

--- CP_2/Lab02/src/myMath/MyMath.java
+++ CP_2/Lab02/src/myMath/MyMath.java
@@ -6,9 +6,16 @@ public class MyMath<T extends Number> {
 	public double getAverage(T[] data) {
 		double _result = 0.0;
 		
+		/*
 		for(int i = 0; i < data.length; i++) {
 			_result += data[i].doubleValue(); // double로 형변환 시키면 오류난다!!
 		}
+		*/
+		
+		for (T k : data) {
+			_result += k.doubleValue();
+		}
+		
 		_result /= data.length;
 		
 		return _result;
